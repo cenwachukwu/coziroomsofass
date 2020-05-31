b74001fa458a36778581d3f13733ea630667b9de
cenwachukwu <cenwachukwu@Familys-iMac.local>
2020-05-30 23:45:54 -0400

    made some changes to category nav

--- src/components/categoryNav/categoryNav.js
+++ src/components/categoryNav/categoryNav.js
@@ -5,23 +5,25 @@ import { Link } from '@reach/router';
 const CategoryNav = (props) => {
   return (
     <div className="categoryNav">
-      <ul className="navbarCategoryLinks">
-        <li>
-          <Link to="/Chair">Chair</Link>
-        </li>
-        <li>
-          <Link to="/AccentChair">Accent Chair</Link>
-        </li>
-        <li>
-          <Link to="/SofaandLoveseat">Sofa & Loveseat</Link>
-        </li>
-        <li>
-          <Link to="/Recliner">Recliners</Link>
-        </li>
-        <li>
-          <Link to="/Sectionals">Sectionals</Link>
-        </li>
-      </ul>
+      <div className="categoryNavContainer">
+        <ul className="navbarCategoryLinks">
+          <li>
+            <Link to="/Chair">Chair</Link>
+          </li>
+          <li>
+            <Link to="/AccentChair">Accent Chair</Link>
+          </li>
+          <li>
+            <Link to="/SofaandLoveseat">Sofa & Loveseat</Link>
+          </li>
+          <li>
+            <Link to="/Recliner">Recliners</Link>
+          </li>
+          <li>
+            <Link to="/Sectionals">Sectionals</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
